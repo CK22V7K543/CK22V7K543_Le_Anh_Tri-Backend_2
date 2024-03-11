36bfd68c854320b412c7a35e06232292c9cc74a4
CK22V7K543 <trick22v7k543@vlvh.ctu.edu.vn>
2024-03-11 09:12:34 +0700

    Cai dat cac handler truy xuat CSDL

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const app = require("./app");
 const config = require("./app/config");
+const mongoDB = require("./app/utils/mongodb.util");
 
+async function startServer(){
+    try{
+        await mongoDB.connect(config.db.uri);
+        console.log("connected to the database!");
 
-const PORT = config.app.port;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
-});
+        const PORT = config.app.port;
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    }catch(error){
+        console.log("Cannot connect to the database!", error);
+        process.exit();
+    }
+}
+
+startServer();
